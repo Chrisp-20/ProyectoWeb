09b4981bc566f16ce860f2486af9017b7f86916e
chris <christian.pereira1@mail.udp.cl>
2025-12-07 18:40:30 -0300

    acvb

--- Backend/services/auth.service.js
+++ Backend/services/auth.service.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const crearToken = (id) => {
-    return jwt.sign({ id }, 'Palabra secreta', { expiresIn: '7d' });
+    return jwt.sign({ id }, 'clave_secreta', { expiresIn: '7d' });
 };
 
 module.exports = { crearToken };
