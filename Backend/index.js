f23e2d907fe0b14c157007177c9f9c74f37dc5d1
chris <christian.pereira1@mail.udp.cl>
2025-12-07 17:38:55 -0300

    mishh

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const authRoutes = require("./routes/auth.routes.js");
 const userRoutes = require("./routes/user.routes.js");
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 mongoose.connect("mongodb+srv://userweb:Us3rweb_@cluster00.zfjs1ub.mongodb.net/?appName=Cluster00")
   .then(() => console.log("MongoDB conectado"))
   .catch(err => console.log("Error Mongo:", err));
@@ -18,6 +20,14 @@ mongoose.connect("mongodb+srv://userweb:Us3rweb_@cluster00.zfjs1ub.mongodb.net/?
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/ruleta", ruletaRoutes);
+
+
+app.use(express.static(path.join(__dirname, "../public"))); 
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html")); 
+});
 
 app.listen(3000, () => {
   console.log("Backend API corriendo en el puerto 3000");
