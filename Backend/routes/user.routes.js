7f05bc42ebda08ee6b360d60bdcc1b43761cd942
chris <christian.pereira1@mail.udp.cl>
2025-12-07 18:29:59 -0300

    xddx

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const { getPerfil } = require("../controllers/user.controller.js");
-const { authMiddleware } = require("../middleware/auth.middleware.js");
+const authMiddleware = require("../middleware/auth.middleware.js");
 
 const router = express.Router();
-
 
 router.get("/perfil", authMiddleware, getPerfil);
 
