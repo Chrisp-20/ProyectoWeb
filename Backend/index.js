f6bc640aaa819c7edd043b5cc22c9faddb2ca37a
chris <christian.pereira1@mail.udp.cl>
2025-12-07 18:32:40 -0300

    aasdadasfaf

--- Backend/index.js
+++ Backend/index.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html")); 
+  res.sendFile(path.join(__dirname, "../public/Ruleta.html")); 
 });
 
 app.listen(3000, () => {
